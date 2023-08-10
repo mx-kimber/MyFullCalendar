5a5f1e78b6874e96ecb42f209f960d577649c3d3
Kimber Ogden <mx.kimber@gmail.com>
2023-08-09 21:49:26 -0500

    Import FullCalendar plugins in the Calendar component, Pass plugins in and set the initial view to show a monthly calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,12 +1,22 @@
-// shortcut: type 'rcfe' to auto generate
-
 import React from 'react'
+
+// import the FullCalender Plugins 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
 
 function Calendar() {
   return (
     <div>
-      <h2>My first FullCalendar</h2>
-      <a href="https://fullcalendar.io/">Visit the FullCalendar website for documentation</a>
+      <FullCalendar
+        // pass in the plugins
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        // set initialView - what you want your index.html to look like when you first load the component
+        initialView={"dayGridMonth"}
+        // now check your index page to see the calender show in month format
+      />
     </div>
   )
 }
