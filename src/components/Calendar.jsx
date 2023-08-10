0b7e0266b25c8a8fdac2bdcf4e039d313482111e
Kimber Ogden <mx.kimber@gmail.com>
2023-08-09 22:32:07 -0500

    Import a new plugin to view the calendar as a list by year, week, day, or month

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-
-// import the FullCalender Plugins 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-
+import listPlugin from '@fullcalendar/list';
 
 function Calendar() {
   return (
     <div>
       <FullCalendar
-        // pass in the plugins
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
-        // set initialView - what you want your index.html to look like when you first load the component
-        initialView={"dayGridMonth"}
-        // now check your index page to see the calender show in month format
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+        // Cool right!? Now check out these views:
+        initialView={"listDay"}
+        
+        // initialView={"dayGrid"}
+        // initialView={"timeGridDay"}
+
+        // import another plugin 
+        //    1. terminal: npm install @fullcalendar/list
+        //    2. import listPlugin into this component (see imports)
+        //    3. add the plugin to plugins
+        //    4. set initialView to either of these: listDay, listWeek,
+        //       listMonth, or listYear.
+        
+        // for the full list of plugins: https://fullcalendar.io/docs/plugin-index
       />
     </div>
   )
