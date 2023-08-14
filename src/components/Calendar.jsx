0f1a42f030e0b479e5adfe9dd5726a981ccadb18
Kimber Ogden <mx.kimber@gmail.com>
2023-08-13 23:37:42 -0500

    Enhanced Calendar with Draggable Events and Options / Import Draggable plugin from @fullcalendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,29 +1,72 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import React, { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 
 function Calendar() {
+  const [removeAfterDrop, setRemoveAfterDrop] = useState(false);
+
+  useEffect(() => {
+    const containerEl = document.getElementById('external-events');
+
+    new Draggable(containerEl, {
+      itemSelector: '.fc-event',
+      eventData: (eventEl) => ({
+        title: eventEl.innerText,
+      }),
+    });
+  }, []);
+
+  const handleDrop = (info) => {
+    console.log("handlingDrop");
+  // note: it keeps dropping a duplicate 
+    if (removeAfterDrop) {
+      info.draggedEl.parentNode.removeChild(info.draggedEl);
+    }
+  };
+
+  const handleCheckboxChange = () => {
+    setRemoveAfterDrop(!removeAfterDrop);
+  };
+
   return (
     <div>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
-        initialView={"dayGridMonth"}
-        headerToolbar={{
-          start: 'prev ,today, next',
-          center: 'title',
-          end: 'dayGridMonth timeGridWeek timeGridDay' 
-        }}
-        //for fun, let's set the height and turn the weekend view off
-        height={"80vh"}
-        weekends={false}
-      />
+      <div id='external-events'>
+        <p>
+          <strong>Draggable Events</strong>
+        </p>
+        <div className='event'>Event 1</div>
+        <div className='event'>Event 2</div>
+        <div className='event'>Event 3</div>
+        <p>
+          <input
+            type='checkbox'
+            id='remove-after-drop-checkbox'
+            checked={removeAfterDrop}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor='remove-after-drop-checkbox'>Remove after drop</label>
+        </p>
+      </div>
+      <div id='calendar-container'>
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+          initialView='dayGridMonth'
+          headerToolbar={{
+            start: 'prev today next',
+            center: 'title',
+            end: 'dayGridMonth timeGridWeek timeGridDay',
+          }}
+          weekends={true}
+          editable={true}
+          droppable={true}
+          drop={handleDrop}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Calendar
-
-// FullCalendar documentation https://fullcalendar.io/docs#toc
+export default Calendar;
